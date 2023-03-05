a2ca91975a16d421fa6e3f16e6f0e71682d66df2
Affani Sanae <affani.sanae@gmail.com>
2023-03-05 15:04:18 +0100

    add_modif

--- src/js/script_modal_payer_agios.js
+++ src/js/script_modal_payer_agios.js
@@ -1,10 +1,21 @@
+/*fonction qui affiche le modal Payer mes agios  */
 function displayModalPayerAgios(event) {
   document
     .querySelector(".overlay_modal_payer_agios")
     .classList.add("show_modal_payer_agios");
+}
+
+/* fonction pour fermer le modal*/
+function removeModal() {
+  document
+    .querySelector(".overlay_modal_payer_agios")
+    .classList.remove("show_modal_payer_agios");
 }
 document
   .querySelector(".button_payer_agios")
   .addEventListener("click", function (event) {
     displayModalPayerAgios();
   });
+document.querySelector(".close").addEventListener("click", function (event) {
+  removeModal();
+});
